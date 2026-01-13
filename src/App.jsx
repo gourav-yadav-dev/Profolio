a6d146c1fe2a7ea1cef774f4cd35af874b8adbad
gourav-yadav-dev <yadavgourav263@gmail.com>
2026-01-13 23:22:15 +0530

    Add contact detail with css and attach img

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Hero } from './components/Hero/Hero'
+import { Contact } from './components/Contact/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Hero />
+      <Contact />
     </>
   )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,42 @@
+import "./Contact.css";
+
 export function Contact() {
     return (
-        <div>
-            contact detail
-        </div>
-    )
+        <section className="contactContainer">
+
+            {/* LEFT SIDE */}
+            <ul className="contactList">
+                <li>
+                    <img src="/public/mail-icon.svg" alt="email" />
+                    <a href="mailto:yadavgourav263@gmail.com">yadavgourav263@gmail.com</a>
+                </li>
+
+                <li>
+                    <img src="/public/call-icon.svg" alt="phone" />
+                    <a href="tel:+9111773835">9111773835</a>
+                </li>
+
+                <li>
+                    <img src="../../../public/web-icon.svg" alt="website" />
+                    <a href="https://www.mywebsite.com" target="_blank" rel="noreferrer">
+                        www.mywebsite.com
+                    </a>
+                </li>
+            </ul>
+
+            {/* RIGHT SIDE */}
+            <div className="socialLinks">
+                <a href="#" target="_blank">
+                    <img src="/public/dribble-icon.svg" alt="dribbble" />
+                </a>
+                <a href="www.linkedin.com/in/gourav-yadavv" target="_blank">
+                    <img src="/public/linkedin-icon.svg" alt="linkedin" />
+                </a>
+                <a href="https://github.com/gourav-yadav-dev" target="_blank">
+                    <img src="/public/facebook-icon.svg" alt="facebook" />
+                </a>
+            </div>
+
+        </section>
+    );
 }
